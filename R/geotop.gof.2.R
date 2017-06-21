c01dd782fbce06781c5c3a302b7757c8e5411509
ecor <emanuele.cordano@gmail.com>
2017-06-21 17:56:45 +0200

    testing gof with status

--- R/geotop.gof.2.R
+++ R/geotop.gof.2.R
@@ -200,7 +200,7 @@ geotopGOF <- function(x=NULL,run.geotop=TRUE,target=NULL,uscale=NULL,when=NULL,g
 	
 		out <- try(do.call(what=geotopExec,args=args[names(args) %in% geotop_exec_names]),silent=TRUE)
 		str(out)
-		proint("HH")
+		print("markpoint")
 	###print("exit exec") COMMENTED BY EC ON 20170207
 	####print(out)
 	}  else {
