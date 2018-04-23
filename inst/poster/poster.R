597e68184b07e667884ccc9cce6fe9c03406c314
ecor <emanuele.cordano@gmail.com>
2018-04-24 00:35:49 +0200

    opdates

--- inst/poster/poster.R
+++ inst/poster/poster.R
@@ -6,6 +6,10 @@ library(webshot)
 
 
 ## https://github.com/odeleongt/postr/pull/9/commits/fac03ce6aa7fb2a0df6f450ff076c0d4f79ca0ec
-rmarkdown::draft("my_poster.Rmd", template = "poster", package = "postr")
-rmarkdown::render("my_poster/my_poster.Rmd")
-postr::render("my_poster.html")
+##rmarkdown::draft("my_poster.Rmd", template = "poster", package = "postr")
+wpath <- '/home/ecor/Dropbox/R-packages/geotopOptim2/inst/poster' 
+
+
+
+rmarkdown::render(sprintf("%s/my_poster/erum_poster.Rmd",wpath))
+postr::render(sprintf("%s/my_poster/erum_poster.html",wpath))
