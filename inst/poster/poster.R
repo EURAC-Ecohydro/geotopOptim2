1e7722893107cba600c2084de6633d7a08edf0cd
Emanuele Cordano <cordaem@d01ri1500655.ies.jrc.it>
2018-04-24 16:49:02 +0200

    poster

--- inst/poster/poster.R
+++ inst/poster/poster.R
@@ -3,7 +3,7 @@
 library(rmarkdown)
 library(flexdashboard)
 library(webshot)
-
+###library(postr)
 
 ## https://github.com/odeleongt/postr/pull/9/commits/fac03ce6aa7fb2a0df6f450ff076c0d4f79ca0ec
 ##rmarkdown::draft("my_poster.Rmd", template = "poster", package = "postr")
@@ -11,5 +11,5 @@ wpath <- '/home/ecor/Dropbox/R-packages/geotopOptim2/inst/poster'
 wpath <- '/STORAGE/projects/R-Packages/geotopOptim2/inst/poster' 
 
 
-rmarkdown::render(sprintf("%s/my_poster/erum_poster.Rmd",wpath))
+rmarkdown::render(sprintf("%s/my_poster/my_poster.Rmd",wpath))
 postr::render(sprintf("%s/my_poster/erum_poster.html",wpath))
